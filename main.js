006d190a7402b2f8b648b74d48fe070cee126053
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-08-11 12:57:10 +0200

    added lazyload

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-var $ = jQuery;
-$(window).on("load", function () {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:55,
-        nav:true,
-        responsive:{
-            0:{
-                items:2
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:4
-            }
-        }
+const lazyload = target =>
+{
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting)
+      {
+        $(entry.target).addClass("fade-out");
+        observer.disconnect();
+      }
     });
+  });
+  io.observe(target);
+}
+
+$(function() {
+  
+  const targets = document.querySelectorAll(".fade");
+  targets.forEach(lazyload);
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:55,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
 });
